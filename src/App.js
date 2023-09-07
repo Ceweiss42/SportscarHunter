c63fe5c1ea9966baba8c3c83c6c2ca6f05236602
Ceweiss42 <ceweiss7@gmail.com>
2023-09-07 16:41:49 -0700

    updates visual

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello there Richard! Is this working?
+          Hello there Everyone! Is this working? It is currently hosted on Github Pages for free! You should be able to "Download" This site by simply hitting Share-> Add to Home Screen
         </p>
         <a
           className="App-link"
